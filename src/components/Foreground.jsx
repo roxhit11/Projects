87e905d227ab460e18771bdbb4e4417392f1c05a
Rohit <rohitbisen24@gmail.com>
2023-12-17 22:31:18 +0530

    Initial commit

---
+++ src/components/Foreground.jsx
@@ -0,0 +1,63 @@
+import { motion } from "framer-motion";
+import Card from "./Card";
+import React, { useRef, useState } from "react";
+
+function Foreground() {
+  const ref = useRef(null);
+
+  const data = [
+    {
+      desc: "Great things happen to those who don't stop believing, trying, learning, and being grateful. ― Roy T. Bennett",
+      filesize: ".5md",
+      close: false,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      }
+    },
+    {
+      desc: "Find out who you are and do it on purpose. — Dolly Parton.",
+      filesize: ".9md",
+      close: false,
+      tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "blue",
+      }
+    },
+    {
+      desc: "Fight for the things that you care about, but do it in a way that will lead others to join you.” —Ruth Bader Ginsburg",
+      filesize: "",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Upload",
+        tagColor: "green",
+      }
+    },
+    {
+      desc: "",
+      filesize: "",
+      close: true,
+      tag: {
+        isOpen: true,
+        tagTitle: "Create",
+        tagColor: "green",
+      },
+    }
+  ];
+
+  return (
+    <div
+      ref={ref}
+      className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5"
+    >
+      {data.map((item, index) => (
+        <Card data={item} reference={ref} />
+      ))}
+    </div>
+  );
+}
+
+export default Foreground;
